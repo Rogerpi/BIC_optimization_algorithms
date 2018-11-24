f05614c3a6df51a230388ed8e386d526bfa92175
RogerPi <u1926981@gmail.com>
2018-11-24 19:34:49 +0000

    swarm struct saves now all the information

--- PSO_standard.m
+++ PSO_standard.m
@@ -17,11 +17,10 @@ function PSO_standard(swarmsize,alpha,beta,gamma,delta,epsilon,inf_ratio,f)
     swarm = {};
     swarm.pose = rand(dimention,swarmsize)*k_rand_pose*2 - k_rand_pose; %TODO: initialize
     swarm.vel = rand(dimention,swarmsize)*k_rand_vel*2 - k_rand_vel;
-    
-    best_swarm = {};
-    best_swarm.pose = swarm.pose;
-    best_swarm.vel = swarm.vel;
-    best_swarm.fitness = repmat(-Inf,1,swarmsize); %first iteration is pointless so far     
+    swarm.mem_pose = swarm.pose;
+    swarm.mem_fitness = repmat(-Inf,1,swarmsize); %first iteration is pointless so far     
+    swarm.best_pose = [];
+    swarm.best_fitness = -Inf;
     toc
     
     %x_star = zeros(dimention,swarmsize);
@@ -36,32 +35,40 @@ function PSO_standard(swarmsize,alpha,beta,gamma,delta,epsilon,inf_ratio,f)
         %disp(swarm.pose)
         %disp(max(swarm.vel(:)))
         %AssesFitness
-        fitness = f(swarm.pose')'; 
+        fitness = f(swarm.pose')';      
         
-        sc = scatter3(swarm.pose(2,:),swarm.pose(1,:),-fitness);
         %Update best particles
         for p =1:swarmsize %TODO vectorize
-            if fitness(p) > best_swarm.fitness(p)
-                best_swarm.fitness(p) = fitness(p);
-                best_swarm.pose(:,p) = swarm.pose(:,p);
+            if fitness(p) > swarm.mem_fitness(p)
+                swarm.mem_fitness(p) = fitness(p);
+                swarm.mem_pose(:,p) = swarm.pose(:,p);
             end
         end
+        %Update best of bests
+        [swarm.best_fitness,best_idx] = max(swarm.mem_fitness);
+        swarm.best_pose = swarm.mem_pose(:,best_idx);
+        
+        %Print
+        sc = scatter3(swarm.pose(2,:),swarm.pose(1,:),-fitness);
+        sc_best = scatter3(swarm.best_pose(2),swarm.best_pose(1),-swarm.best_fitness,'MarkerEdgeColor',[0 .5 .5],'MarkerFaceColor',[0 .7 .7]);
+
+        
         %fill x_star
-        x_star = best_swarm.pose;
+        x_star = swarm.mem_pose;
         %fill x_plus. Select gamma random particles and get the best  
         for p=1:swarmsize
             %random permutation that includes the current particle
             others = [1:p-1,p+1:swarmsize];
             idx = [randperm(swarmsize-1,n_inf),p];
             %get best fitness p
-            [~,best_idx] = max(best_swarm.fitness(:,idx));
+            [~,best_idx] = max(swarm.mem_fitness(:,idx));
             %get best particle
-            x_plus(:,p) = best_swarm.pose(:,best_idx);
+            x_plus(:,p) = swarm.mem_pose(:,best_idx);
 
         end
         %fill x_excl
-        [~,best_idx] = max(best_swarm.fitness);
-        x_excl = repmat(best_swarm.pose(:,best_idx),1,swarmsize);
+        x_excl = repmat(swarm.best_pose,1,swarmsize);
+        
         
         %Get b, c, d
         b = rand(dimention,1)*beta;
@@ -78,6 +85,7 @@ function PSO_standard(swarmsize,alpha,beta,gamma,delta,epsilon,inf_ratio,f)
 
         pause(0.2);
         delete(sc);
+        delete(sc_best);
         
     end
 end
