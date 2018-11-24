2148db90608a6566f8cf68a81d6661d02e2e5234
RogerPi <u1926981@gmail.com>
2018-11-24 16:50:07 +0000

    Genetic Algorithm first commit

---
+++ GA.m
@@ -0,0 +1,51 @@
+function GA(popsize,f)
+    max_it = 400;
+    dimention = 2; %y = f(a,b);
+    k_rand_pose = 100;
+    
+    %Algorithm initialization
+    
+    hold on
+    fprintf("Init Individuals...");
+    
+    people = {};
+    people.pose = rand(dimention,popsize)*k_rand_pose;
+    people.fitness = [];
+    people.best_pose = [];
+    people.best_fitness = -Inf;
+    
+    Q = zeros(dimention,popsize);
+    fprintf("Done!\n");
+    for it=1:max_it
+        people.fitness = f(people.pose')';
+        sc = scatter3(people.pose(2,:),people.pose(1,:),-fitness);
+        for p = 1:popsize 
+            if people.fitness(p) > people.best_fitness
+                people.best_fitness = people.fitness(p);
+                people.best_pose = people.pose(p);
+            end
+
+        end
+        for p = 1:2:round(popsize)
+            Pa = selectWithReplacement(people);
+            Pb = selectWithReplacement(people);
+            [Ca,Cb] = Crossover(Pa,Pb);
+            Q(:,p) = Ca;
+            Q(:,p+1) = Cb;
+        end
+        
+           
+        pause(0.2);
+        delete(sc);
+        
+    end
+  
+end
+
+function Ps = selectwithReplacement(people)
+
+end
+
+function [Ca,Cb] = Crosover(Pa,Pb)
+
+end
