0e5d4773da571deaafb88c4ffcf4128e8d8db2bd
RogerPi <u1926981@gmail.com>
2018-11-24 18:48:01 +0000

    Mutation based on random noise addition

--- GA.m
+++ GA.m
@@ -1,7 +1,8 @@
 function GA(popsize,f)
-    max_it = 400;
+    max_it = 5000;
     dimention = 2; %y = f(a,b);
-    k_rand_pose = 100;
+    k_rand_pose = 10;
+    t_size = 2;
     
     %Algorithm initialization
     
@@ -17,25 +18,27 @@ function GA(popsize,f)
     Q = zeros(dimention,popsize);
     fprintf("Done!\n");
     for it=1:max_it
+        fprintf("Iteration: %d \n",it);
         people.fitness = f(people.pose')';
-        sc = scatter3(people.pose(2,:),people.pose(1,:),-fitness);
+        sc = scatter3(people.pose(2,:),people.pose(1,:),-people.fitness);
         for p = 1:popsize 
             if people.fitness(p) > people.best_fitness
                 people.best_fitness = people.fitness(p);
-                people.best_pose = people.pose(p);
+                people.best_pose = people.pose(:,p);
+                fprintf("Best found \n");
             end
 
         end
         for p = 1:2:round(popsize)
-            [Pa,idx_a] = TournamentSelection(people);
-            [Pb,idx_b] = TournamentSelection(people);
+            [Pa,idx_a] = TournamentSelection(people,t_size);
+            [Pb,idx_b] = TournamentSelection(people,t_size);
             [Ca,Cb] = Crossover(Pa,Pb);
-            Q(:,idx_a) = Mutate(Ca);
-            Q(:,idx_b) = Mutate(Cb);
+            people.pose(:,idx_a) = Mutate(Ca);
+            people.pose(:,idx_b) = Mutate(Cb);
         end
         
            
-        pause(0.2);
+        pause(0.05);
         delete(sc);
         
     end
@@ -47,7 +50,7 @@ function [Ps,idx] = TournamentSelection(people,t)
     len = size(people.pose,2);
     sel = randsample(len,t,true); % select randomly with replacement
     idx = sel(1);
-    for i=2:len
+    for i=2:t
         if people.fitness(sel(i)) > people.fitness(idx)
            idx = sel(i); 
         end
@@ -72,5 +75,11 @@ function [Ca,Cb] = Crossover(Pa,Pb)
 end
 
 function M = Mutate(C)
-    [M,~] = Crossover(C,C);
+    M = C;
+    for i=1:size(C)
+        x = rand(2,1);
+        if x(1) > 0.5
+            M(i) = M(i) + x(2)*2-1;
+        end
+    end
 end
